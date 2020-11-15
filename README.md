de6ad89c408e385d716cda035c9f2ab54ebd33e4
stmichael <webmaster007@yahoo.co.uk>
2020-11-15 16:14:04 +0100

    Tab project using react and vanilla css

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Tab
 A Minor React Project
+
+Completed the "Tab Project", another minor project built using the React Library
+
+In this project I fetched data from an API using the asyn-await function, this data
+is then rendered to browser using the react functional component. In this project I made 
+used of the react useState and useEffect hook.
+Below is the implementation of the "tab" project
+
+# Desktop View of the Tab project 
+
+![First page](./public/images/1.png)
+
+![second page](./public/images/2.png)
+
+
+# Mobile View of the Tab Project
+
+![mobile view image](./public/images/mobile-view.png)
---
+++ src/App.js
@@ -0,0 +1,67 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+import {FaAngleDoubleRight} from 'react-icons/fa'
+
+const url = 'https://course-api.com/react-tabs-project'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url)
+    const jobs = await response.json()
+    setJobs(jobs)
+    setLoading(false)
+  }
+  useEffect(()=>{
+    fetchJobs();
+  }, [])
+  
+  if(loading){
+    return (
+    <div className="App">
+      <h1>Loading....</h1>
+    </div>
+    )
+  }
+
+  const { id, company, dates, duties, title} = jobs[value]
+  return (
+    <div className="App">
+      <div className="tab-title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+      <section className="container">
+        <div className="btn-container">
+          {jobs.map((job, index)=>{
+            return (
+            <div key={job.id}>
+              <button onClick={() => setValue(index)} className={`${index === value ? 'active-btn' : 'btn'}` }>{job.company}</button> 
+            </div>
+            )
+          })}
+        </div>
+        <div className="details">
+          <h3>{title}</h3>
+          <h4 className="company">{company}</h4>
+          <p>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+            <div key={index} className="duty">
+              <p className="icon"><FaAngleDoubleRight /> </p>
+              <p className="duty-p">{duty}</p>
+            </div>
+            )
+          })}
+        </div>
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
